2e053775a8993fbb09a7f09e4e89daf47e8e9d60
Galder Zamarreño <galder@ibm.com>
2026-06-16 17:42:53 +0200

    maintainers: add galder

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -8533,6 +8533,12 @@
     githubId = 168143489;
     keys = [ { fingerprint = "1234 3726 9042 01F3 CE07  59BF A3B6 1E91 5508 F702"; } ];
   };
+  galder = {
+    email = "galder@ibm.com";
+    github = "galderz";
+    githubId = 50187;
+    name = "Galder Zamarreño";
+  }
   gamb = {
     email = "adam.gamble@pm.me";
     github = "gamb";
